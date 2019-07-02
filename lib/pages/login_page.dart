e5df66ab4466183055dbc1b6e81094d9c5bab0f4
Jay Bryant Casilang <jay.casilang@ffuf.de>
2019-07-02 16:07:28 +0800

    Refactored login page.

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,108 +1,120 @@
-import 'package:first_flutter_app/pages/login_view_model.dart';
+import 'package:first_flutter_app/redux/actions/api_actions.dart';
 import 'package:first_flutter_app/redux/actions/auth_actions.dart';
 import 'package:first_flutter_app/state/app_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
 
-class LoginPage extends StatefulWidget {
+class LoginPage extends StatelessWidget {
   static final String ROUTE_NAME = "/login";
-  @override
-  State<StatefulWidget> createState() => LoginPageState();
+  final double padding = 20;
 
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: Text("Login"),
+    ),
+    body: Container(
+      padding: EdgeInsets.all(padding),
+      child: StoreConnector<AppState, LoginViewModel>(
+        builder: (context, viewModel) => Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            TextField(
+              enabled: viewModel.loginEnabled,
+              decoration: InputDecoration(
+                hintText: "Type your username here",
+                labelText: "Username",
+                errorText: viewModel.usernameError,
+              ),
+              maxLines: 1,
+              onChanged: (text) {
+                print('Username $text');
+                viewModel.validateUsername(text);
+              },
+            ),
+            TextField(
+              enabled: viewModel.loginEnabled,
+              decoration: InputDecoration(
+                hintText: "Type your password here",
+                labelText: "Password",
+                errorText: viewModel.passwordError,
+              ),
+              maxLines: 1,
+              onChanged: (text) {
+                print('Password $text');
+                viewModel.validatePassword(text);
+              },
+              obscureText: true,
+            ),
+            RaisedButton(
+                onPressed: viewModel.login,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(viewModel.buttonLabel)
+                  ],
+                )
+            ),
+            FlatButton(
+                onPressed: viewModel.goToRegister,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Register")
+                  ],
+                )
+            ),
+          ],
+        ),
+        converter: (store) => LoginViewModel.fromStore(store),
+      ),
+    ),
+  );
 }
 
-class LoginPageState extends State<LoginPage> {
+class LoginViewModel {
+  final String username;
+  final String password;
+  final String usernameError;
+  final String passwordError;
+  final String buttonLabel;
+  final Function(String) validateUsername;
+  final Function(String) validatePassword;
+  final Function login;
+  final Function goToUserList;
+  final Function goToRegister;
+  final bool loginEnabled;
 
-  @override
-  void initState() {
-    super.initState();
-  }
+  LoginViewModel({
+    this.username,
+    this.password,
+    this.usernameError,
+    this.passwordError,
+    this.buttonLabel,
+    this.validateUsername,
+    this.validatePassword,
+    this.login,
+    this.goToUserList,
+    this.goToRegister,
+    this.loginEnabled,
+  });
 
-  void showErrorDialog(BuildContext context) {
-    showDialog(context: context, barrierDismissible: true, builder: (BuildContext context) => AlertDialog(
-      title: Text("Error"),
-      content: Text("Something went wrong."),
-      actions: <Widget>[
-        FlatButton(onPressed: () {
-          Navigator.of(context).pop();
-        }, child: Text("Ok"),)
-      ],
-    ));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Login"),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(20),
-        child: StoreConnector<AppState, LoginViewModel>(
-          onInit: (store) {},
-          builder: (context, viewModel) => Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-
-              TextField(
-                enabled: viewModel.buttonLabel.toLowerCase() == "login",
-                decoration: InputDecoration(
-                  hintText: "Type your username here",
-                  labelText: "Username",
-                  errorText: viewModel.usernameError,
-                ),
-                maxLines: 1,
-                onChanged: (text) {
-                  print('Username $text');
-                  viewModel.validateUsername(text);
-                },
-              ),
-
-              TextField(
-                enabled: viewModel.buttonLabel.toLowerCase() == "login",
-                decoration: InputDecoration(
-                  hintText: "Type your password here",
-                  labelText: "Password",
-                  errorText: viewModel.passwordError,
-                ),
-                maxLines: 1,
-                onChanged: (text) {
-                  print('Password $text');
-                  viewModel.validatePassword(text);
-                },
-                obscureText: true,
-              ),
-
-              RaisedButton(
-                  onPressed: viewModel.buttonLabel.toLowerCase() == "login" ? viewModel.login : null,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(viewModel.buttonLabel)
-                    ],
-                  )
-              ),
-
-              FlatButton(
-                  onPressed: () {
-                    viewModel.goToRegister();
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text("Register")
-                    ],
-                  )
-              ),
-
-            ],
-          ),
-          
-          converter: (store) => LoginViewModel.fromStore(store),
-
-        ),
-      ),
+  static LoginViewModel fromStore(Store<AppState> store) {
+    return LoginViewModel(
+      username: store.state.authState.username,
+      password: store.state.authState.password,
+      usernameError: store.state.authState.usernameError,
+      passwordError: store.state.authState.passwordError,
+      buttonLabel: store.state.authState.loginButtonLabel,
+      validateUsername: (username) => store.dispatch(ValidateUsername(username)),
+      validatePassword: (password) => store.dispatch(ValidatePassword(password)),
+      login: store.state.authState.loginButtonLabel.toLowerCase() == "login" ?
+          () => store.dispatch(Login(store.state.authState.username, store.state.authState.password)) : null,
+      goToUserList: () => store.dispatch(GoToUserList()),
+      goToRegister: () => store.dispatch(GoToRegister()),
+      loginEnabled: store.state.authState.loginButtonLabel.toLowerCase() == "login",
     );
   }
+
 }
